43afd04234c655263271f93c4b8c2dd2c99fb3f7
Nate <ntkramer@iastate.edu>
2017-09-06 23:18:51 -0500

    did a lot of work setting up google api stuff for location management.

--- GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/CourseActivity.java
+++ GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/CourseActivity.java
@@ -2,11 +2,15 @@ package com.example.nate.golfonthego;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,14 +18,50 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class CourseActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CourseActivity extends FragmentActivity implements OnMapReadyCallback,
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
+    // main google map object
     private GoogleMap mMap;
+    // api for getting location
+    private FusedLocationProviderApi fusedLocationProvider = LocationServices.FusedLocationApi;
+    //api for google api's lol
+    private GoogleApiClient googleApiClient;
+    // for getting locations
+    private LocationCallback locationCallback = new LocationCallback();
+    // the actual location variable
+    Location currentLocation;
 
+    // request for location
+    LocationRequest locationRequest;
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    protected void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    // main on create of the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.createLocationRequest();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        //set the map view
         setContentView(R.layout.activity_course);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -29,18 +69,31 @@ public class CourseActivity extends FragmentActivity implements OnMapReadyCallba
         mapFragment.getMapAsync(this);
     }
 
+    // overriden to start the googleapiclient
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    // overriden to stop the googleapiclient
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
         boolean mapStyleSuccess = mMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.map_style)));
 
@@ -48,7 +101,6 @@ public class CourseActivity extends FragmentActivity implements OnMapReadyCallba
             Context context = getApplicationContext();
             CharSequence text = "Style Input Failed";
             int duration = Toast.LENGTH_SHORT;
-
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
@@ -57,5 +109,40 @@ public class CourseActivity extends FragmentActivity implements OnMapReadyCallba
         LatLng ames = new LatLng(42.02672222, -93.6475);
         mMap.addMarker(new MarkerOptions().position(ames).title("Hopefully Ames"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ames, (float)19.0));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Context context = getApplicationContext();
+        CharSequence text = "Connection Failed";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        try {
+            fusedLocationProvider.requestLocationUpdates(googleApiClient, locationRequest,
+                    locationCallback,
+                    null /* Looper */);
+        } catch(SecurityException e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+        currentLocation = location;
+        LatLng tmp = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(tmp).title("You might be here!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tmp, (float)19.0));
     }
 }
