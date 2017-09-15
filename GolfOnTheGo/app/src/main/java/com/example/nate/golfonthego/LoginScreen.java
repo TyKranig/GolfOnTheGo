d5e2437661045e123f1ed15234665b566e704dec
tkranig <tkranig@iastate.edu>
2017-09-14 20:40:26 -0500

    got php working, currently java calls php, php calls sql, php get a response then returns a json string

--- GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/LoginScreen.java
+++ GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/LoginScreen.java
@@ -4,11 +4,20 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static com.example.nate.golfonthego.R.string.login;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -41,6 +50,8 @@ public class LoginScreen extends AppCompatActivity {
 
     class URLTask extends AsyncTask<Void,Void,Void>{
 
+        public ArrayList<String> userNames;
+
         protected void onPreExecute() {
             //display progress dialog.
         }
@@ -54,6 +65,25 @@ public class LoginScreen extends AppCompatActivity {
                 urlconnection.setDoOutput(true);
                 urlconnection.setRequestMethod("POST");
                 urlconnection.connect();
+                int status = urlconnection.getResponseCode();
+                switch (status) {
+                    case 200:
+                    case 201:
+                        BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line+"\n");
+                        }
+                        br.close();
+                        JSONArray users = new JSONArray(sb.toString());
+                        for (int i = 0; i < users.length(); ++i) {
+                            final JSONObject person = users.getJSONObject(i);
+                            System.out.println(person.getInt("userId"));
+                            System.out.println(person.getString("userName"));
+                            System.out.println(person.getString("password"));
+                        }
+                }
                 // add more code here to send a run request ?
                 urlconnection.disconnect();
             }
