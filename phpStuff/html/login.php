e5741be64ea5f74e27e8f5f05d0319bc5793959c
tkranig <tkranig@iastate.edu>
2017-10-02 21:03:07 -0500

    updated the php to be more modular

--- phpStuff/html/login.php
+++ phpStuff/html/login.php
@@ -1,50 +1,45 @@
 <?php
+include('dbConnection.php');
+
 error_reporting(-1);
 ini_set('display_errors','On');
-$host="mysql.cs.iastate.edu";
-$port=3306;
-$socket="";
-$user="dbu309amc1";
-$password="XFsBvb1t";
-$dbname="db309amc1";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-or die ('Could not connect to the database server' . mysqli_connect_error());
-
-if($con->connect_errno){
-echo "<p>encountered an error connecting</p>";
-}
 
 $userName = $_GET["userName"];
 $pass = $_GET["password"];
 
-$query = "Select password from user where user.userName = " . $userName;
+login($userName, $pass);
 
-$result = $con->query($query)
-or trigger_error($con->error);
+function login($userName, $pass){
+    global $con;
+    $query = "Select password from user where user.userName = " . $userName;
 
-if($result->num_rows != 0){
-   $rows = array();
+    $result = $con->query($query)
+            or trigger_error($con->error);
 
-   while($row = $result->fetch_array(MYSQL_BOTH)){
-      $rows[] = $row;
-   }
+    if($result->num_rows != 0){
+        $rows = array();
 
-   $success = 0;
-   $pass = str_replace("\"", "", $pass);
+        while($row = $result->fetch_array(MYSQL_BOTH)){
+            $rows[] = $row;
+        }
 
-   if($rows[0]['password'] == $pass){
-     $success = 1;
-   }	
+        $success = 0;
+        $pass = str_replace("\"", "", $pass);
+
+        if($rows[0]['password'] == $pass){
+            $success = 1;
+        }	
 	
+    }
+    else{
+        $success = 0;
+    }		
+
+    $data = [ 'result' => $success];
+
+    echo json_encode($data);
+
+    $con->close();
 }
-else{
-     $success = 0;
-}		
-
-$data = [ 'result' => $success];
-
-echo json_encode($data);
-
-$con->close();
 ?>
