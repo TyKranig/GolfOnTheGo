e6cf295c4fc253abf9b724e41719ed6210ccc23d
rquigley <rquigley@iastate.edu>
2017-10-05 23:57:07 -0500

    Generic LeaderBoard with random scores

--- GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/LeaderActivity.java
+++ GolfOnTheGo/app/src/main/java/com/example/nate/golfonthego/LeaderActivity.java
@@ -1,11 +1,13 @@
 package com.example.nate.golfonthego;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class LeaderActivity extends AppCompatActivity {
     //TableLayout ll = (TableLayout) findViewById(R.id.Table);
@@ -13,31 +15,58 @@ public class LeaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader);
-        //initTable();
+        initTable();
     }
 
+
     public void initTable(){
-        //ll.setOutlineProvider();
 
+        //Find Table
+        TableLayout tl=(TableLayout)findViewById(R.id.Table);
+
+        String[] names = {"NAME:", "Stan", "Dave", "Matt", "Larry", "Joseph", "Danny", "Luke", "Zach", "John"};
+        Random rand = new Random(System.currentTimeMillis());
         for(int i=0; i<10; i++) {
-            TableRow row = new TableRow(this);
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            row.setLayoutParams(lp);
-            CheckBox checkBox = new CheckBox(this);
-            TextView tv = new TextView(this);
-            ImageButton addBtn = new ImageButton(this);
-            //addBtn.setImageResource(R.drawable.add);
-            ImageButton minusBtn = new ImageButton(this);
-            //minusBtn.setImageResource(R.drawable.minus);
-            TextView qty = new TextView(this);
-            checkBox.setText("hello");
-            qty.setText("10");
-            row.addView(checkBox);
-            row.addView(minusBtn);
-            row.addView(qty);
-            row.addView(addBtn);
-            //ll.addView(row,i);
+            //create a new row
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
+            //add text for name
+            TextView tvn = new TextView(this);
+            tvn.setPadding(10,0,0,0);
+            tvn.setText(names[i]);
+            //tvn.setText("asdf");
+
+            //textview.getTextColors(R.color.)
+            tvn.setTextColor(Color.BLACK);
+
+            //add name into the row
+            tr.addView(tvn);
+
+            //buffer space
+            TextView tvb = new TextView(this);
+            tvb.setPadding(10,0,0,0);
+            tvb.setText("  ");
+            tr.addView(tvb);
+
+            //add text for score
+            TextView tvs = new TextView(this);
+            if(i==0) tvs.setText("SCORE:");
+            //else tvs.setText("asdf");
+            else{
+                int score = rand.nextInt()%999+1;
+                if(score<1) score = score *-1;
+                tvs.setText(Integer.toString(score));
+            }
+
+            //add score into the row
+            tr.addView(tvs);
+
+
+            //add row into table
+            tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
         }
+
+
     }
 }
